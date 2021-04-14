4debf1b6c284ca4713a2956601783a5597607458
David Magazine <crashrz3@gmail.com>
2021-04-14 12:54:32 -0400

    initial commit

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '87995530e2b440658e6fbd27105b8568',
+        ClientSecret: '686651dd79ad4312aa56bd55fb99056a'
+    }
+}
+
+export { Credentials };
